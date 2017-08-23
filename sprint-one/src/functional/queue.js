06c81a00c8bcd65e09f09a48550e03ec737bb3c7
davirk21 <davirk21@gmail.com>
2017-08-23 15:31:25 -0700

    Queue: report a size of 0 after removing more items than adding

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,23 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  var size = 0;
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size ++;
   };
+  
 
   someInstance.dequeue = function() {
+    if (size > 0) {
+      size--;
+    } else {
+      size;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
