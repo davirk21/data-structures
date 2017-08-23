6377eed78b057fa676ac73700fcea9200ef0f27f
davirk21 <davirk21@gmail.com>
2017-08-23 16:42:37 -0700

    The functional- shared pattern: does not error when removing from empty stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+
+  storage.count = 0;
+  
+  // storage.push = stackMethods.push;
+  // storage.pop = stackMethods.pop; //barrow methods
+  // storage.size = stackMethods.size;
+
+  _.extend(storage, stackMethods);
+  
+  return storage;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function() {
+  this.count++;
+};
 
+stackMethods.pop = function() {
+
+};
+
+stackMethods.size = function() {
+  return this.count;
+}; //method to delegate fallback methods
+
+console.log(stackMethods);
