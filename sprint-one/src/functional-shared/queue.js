3cb6669e810b8f9ad9e88726a260bb453e8f4a51
davirk21 <davirk21@gmail.com>
2017-08-23 18:05:47 -0700

    size is 0 after removing more item then adding

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  
+  instance.count = 0;
+  
+  _.extend(instance, queueMethods);
+  
+  return instance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  this.count++;
 
+};
+
+queueMethods.dequeue = function () {
+  if (this.count > 0) {
+    this.count--;
+  } else {
+    this.count;
+  }
+};
+
+queueMethods.size = function () {
+  return this.count;
+}; 
