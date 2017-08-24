5edb44bf35d236999c22fcce4c9b9deb41c21be7
davirk21 <davirk21@gmail.com>
2017-08-24 10:09:35 -0700

    Finsihed stack shared behavior and stack- specific behaviors

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -2,15 +2,14 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   this.count = 0;
+  this.result = {};
 
 };
 
-Stack.prototype.size = function() {
-  return this.count;
-};
 
 
-Stack.prototype.push = function() {
+Stack.prototype.push = function(value) {
+  this.result[this.count] = value;
   return this.count++;
 };
 
@@ -21,7 +20,15 @@ Stack.prototype.pop = function() {
   } else {
     this.count;
   }
+  var popped = this.result[this.count];
+  delete this.result[this.count];
+  return popped;
 };
+
+Stack.prototype.size = function() {
+  return this.count;
+};
+
 
 
 
