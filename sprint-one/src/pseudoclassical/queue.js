9fc567d8b86a56bc791a50f5539eb5ed12735b29
davirk21 <davirk21@gmail.com>
2017-08-24 10:23:25 -0700

    report size 0 if removing more than adding

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -2,9 +2,24 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   this.count = 0;
+  this.result = {};
 
 };
 
 Queue.prototype.size = function() {
   return this.count;
+};
+
+Queue.prototype.enqueue = function(value) {
+  
+  return this.count++;
+  
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.count > 0) {
+    return this.count--;
+  } else {
+    return this.count;
+  }
 };
