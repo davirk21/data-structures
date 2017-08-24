8eb18c52073d1197d8b81590b64fd0adcd2c0dbd
davirk21 <davirk21@gmail.com>
2017-08-23 19:39:27 -0700

    reports a size of zero for a new stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,16 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var storage = Object.create(stackMethods);
+  storage.count = 0;
+  
+  return storage;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  return this.count;
+}
 
