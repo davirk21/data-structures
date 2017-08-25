a95b630bb8e10f757dbe8f4394d7878af64a054c
davirk21 <davirk21@gmail.com>
2017-08-25 14:42:37 -0700

    finished set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  var index = _.indexOf(this._storage, item, false);
+  this._storage.splice(index, 1);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ 
