6a1d92c7b0eadc3e0658033e825e6468fa288366
davirk21 <davirk21@gmail.com>
2017-08-25 11:25:52 -0700

    Finished tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,20 +1,55 @@
 var Tree = function(value) {
   var newTree = {};
-  newTree.value = value;
+  newTree.value = value; 
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //pudh add child into .children array
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  
+  console.log(this);
+  //create a helper function to recurse contains
+  var test = function (node, target) { 
+      // if the value of tree equals target...
+      if (node.value === target) {
+        //return true;
+        return true;
+
+      // //else if the first children does exist...
+      // } else if (node.children.length > 0) {
+      //     //run .contains for tree.children
+      //   _.filter(node.children, function(child) {
+      //     if (test(child, target)) {
+      //       return true;
+      //     }
+      // } 
+
+      } else if (node.children[0] !== undefined) {
+        for (var i = 0; i < node.children.length; i++){
+          if (test(node.children[i], target)){
+            return true;
+          }
+        }
+      }
+       
+     return false;   
+  };
+
+  return test(this, target);
 };
+
+
+
 
 
 
