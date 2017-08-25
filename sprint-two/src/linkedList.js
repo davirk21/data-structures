1c2cf014e50c1ba6062f7a138e631c68aa9176ff
davirk21 <davirk21@gmail.com>
2017-08-24 22:23:08 -0700

    finished linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,74 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  
+
+
   list.addToTail = function(value) {
+    if (this.head === null) {
+      this.head = Node(value);
+      this.tail = this.head;
+    } else {
+      var place = this.head;
+      while (place.next !== null) {
+        place = place.next;
+      }
+      
+      place.next = Node(value);
+      this.tail = place.next;
+    }
+    
   };
 
   list.removeHead = function() {
+      //head will equal the next value of head;
+    var remove = this.head.value;
+    this.head = this.head.next;
+    return remove;
+
   };
 
   list.contains = function(target) {
+    var place = this.head;
+    
+    var helper = function (place, target) {
+      if (place.value === target) {
+        return true;
+      } else if (place.next !== null)  {
+        return helper(place.next, target);       
+      }
+      return false;
+    };  
+
+    return helper(place, target);
+  
+    // var test = function(head, num) {
+    //   if (head.value === num) {
+    //     return true;
+    //   } else if (head.next !== null) {
+    //     return test(head.next, num);
+    //   }
+    //   return false;
+    // };
+    
+    // return test(place, target);
+    
+
+    // while (place.next !== null) {
+    //   if (place.value === target) {
+    //     return true;
+    //   } else {
+    //     place = place.next;
+    //   }
+    // }
+    // if (place.value === target) {
+    //   return true;
+    // }
+    // return false;
+
+    // if (place.value === target){
+    //   return value;  
+    // }
   };
 
   return list;
