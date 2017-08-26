6f2b861180a49bf3f51b613ef6d43d35fd2a1aff
davirk21 <davirk21@gmail.com>
2017-08-26 16:28:07 -0700

    Finish Hash Tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,19 +3,57 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-};
+}; 
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, []);
+    this._storage.get(index).push([k, v]);
+  } else {
+    var storage = this._storage.get(index);
+    var truth = false;
+    var place = 0;
+    for (var i = 0; i < storage.length; i++) {
+      var tuple = storage[i];
+      if (tuple[0] === k) {
+        truth = true;
+      }
+    }
+    if(truth) {
+      this._storage.get(index).splice(place, 1);
+      this._storage.get(index).push([k, v]);
+    } else {
+      this._storage.get(index).push([k, v]);
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var storage = this._storage.get(index)
+  for (var i = 0; i < storage.length; i++){
+    var tuple = storage[i];
+    if (tuple[0] === k) {
+      return tuple[1];
+    }
+  }
+  
+  return undefined;
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-};
+  var storage = this._storage.get(index)
+  for (var i = 0; i < storage.length; i++){
+    var tuple = storage[i];
+    if (tuple[0] === k) {
+      this._storage.get(index).splice(i, 1)
+    }
+  }
+  
+}; 
 
 
 
